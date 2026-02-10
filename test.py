dd488be54e33e3611cd38c6504ec1dc8f0db59ce
Abdullah2265Habib <abdullah2006habib@gmail.com>
2026-02-10 18:58:23 +0500

    loaded the audio file

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import os
+import numpy as np
+import librosa
+import soundfile as sf
 
 input_folder = "audio"
 output_folder = "output"
@@ -6,4 +9,6 @@ audio_file = os.path.join(input_folder, "sound.mp4")
 
 os.makedirs(output_folder, exist_ok=True)
 
-
+#extract audio
+signal, fs = librosa.load(audio_file, sr=None, mono=True)
+t = np.linspace(0, len(signal) / fs, len(signal), endpoint=False)
